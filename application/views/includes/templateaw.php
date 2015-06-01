d24f2427bc9e6989d34179c161d7fbe38901d73d
tryitfirst <tugastok7@yahoo.com>
2015-06-01 11:27:33 +0700

    Revert "Revert "Revert "Perizinan"""
    This reverts commit fde7473b115e62315e4ad2214e6a5f9f38284741.

--- application/views/includes/templateaw.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('includes/headeraw'); ?>
-
-<?php $this->load->view($main_content); ?>
-
-<?php $this->load->view('includes/footeraw'); ?>
--- application/views/signup_successful.php
+++
@@ -1,2 +0,0 @@
-<h1>Selamat!</h1>
-<p><center>Akun telah dibuat. <?php echo anchor('login','Login Now')?></center>
